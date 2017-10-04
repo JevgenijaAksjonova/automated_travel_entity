a3f4da57a27e9c350a21e473da9f3cef9d3a4d1d
Oskar Andersson <oanders@kth.se>
2017-10-04 10:57:07 +0200

    working on the pose equations, think they are correct now. Need to work on transforms next

--- odometry/src/Odometry_v1.cpp
+++ odometry/src/Odometry_v1.cpp
@@ -2,6 +2,7 @@
 #include <ras_lab1_msgs/PWM.h>
 #include <ras_lab1_msgs/Encoders.h>
 #include <nav_msgs/Odometry.h>
+#include <tf/transform_broadcaster.h>
 #include <math.h>
 
 
@@ -9,22 +10,30 @@
  * This tutorial demonstrates simple sending of messages over the ROS system.
  */
 
-class Odometry_v1{
+class OdometryPublisher{
 public:
     ros::NodeHandle n;
     ros::Publisher odom_publisher;
-    ros::Subscriber twist_subscriber;
     ros::Subscriber encoder_subscriber;
-
-
-struct current_pose{
-    double x;
-    double y;
-    double theta;
+    OdometryPublisher(int control_frequency); //constructor
+    void calculateNewPosition();
+private:
+    std::vector<int> encoding_abs;
+    std::vector<double> encoding_delta;
+    int control_frequency;
+    void encoderCallback(const ras_lab1_msgs::Encoders::ConstPtr &msg);
+    struct current_pose{
+        double x;
+        double y;
+        double theta;
+    };
 };
 
-Odometry_v1(){
 
+
+
+OdometryPublisher::OdometryPublisher(int frequency){
+    control_frequency = frequency;
     n = ros::NodeHandle("~");
     current_pose.x = 0;
     current_pose.y = 0;
@@ -33,46 +42,75 @@ Odometry_v1(){
     encoding_abs = std::vector<int>(2,0);
     encoding_delta = std::vector<double>(2,0);
 
-    odom_publisher = n.advertise<nav_msgs::Odometry>("/kobuki/odom_v1", 1);
+    odom_publisher = n.advertise<nav_msgs::Odometry>("odom", 1);
     encoder_subscriber = n.subscribe("/kobuki/encoders", 1, &StraightWalker::encoderCallback, this);
 
 
 }
 
 
-void encoderCallback(const ras_lab1_msgs::Encoders::ConstPtr& msg){
+void OdometryPublisher::encoderCallback(const ras_lab1_msgs::Encoders::ConstPtr& msg){
     encoding_abs[0] = msg->encoder1;
     encoding_abs[1] = msg->encoder2;
 
     encoding_delta[0] = (double) (msg->delta_encoder1);
     encoding_delta[1] = (double) (msg->delta_encoder2);
 
-//    ROS_INFO("Absolute encoding values [%d], [%d]\n", encoding_abs[0], encoding_abs[1]);
-//    ROS_INFO("delta encoding values [%d], [%d]\n", encoding_delta[0], encoding_delta[1]);
-
 }
 
 
-void calculateNewPosition(){
+void OdometryPublisher::calculateNewPosition(){
     double wheel_r = 0.0352;
     double base_d = 0.23;
     double pi = 3.1416;
     double tick_per_rotation = 360;
     double control_frequenzy = 10; //10 hz
-    nav_msgs::Odometry ;
+    double dt = 1/control_frequenzy;
 
-    //convert to radians
+    ros::Time current_time, last_time;
+
     std::vector<double> estimated_w(2, 0.0);
 
-    estimated_w[0] = (encoding_delta[0]*2*pi*control_frequenzy)/(tick_per_rotation);
-    estimated_w[1] = (encoding_delta[1]*2*pi*control_frequenzy)/(tick_per_rotation);
+    estimated_w[0] = (encoding_delta[0])/(tick_per_rotation)*wheel_r*2*pi;
+    estimated_w[1] = (encoding_delta[1])/(tick_per_rotation)*wheel_r*2*pi;
 
-    double linear_v = ((estimated_w[0] + estimated_w[0])/2)*wheel_r;
-    double angular_v = (estimated_w[0] - estimated_w[0])*wheel_r/base_d;
+    double linear_v = ((estimated_w[0] + estimated_w[1])/2)/dt;
+    double angular_v = ((estimated_w[0] - estimated_w[1])/base_d)/dt;
 
-    current_pose.x = current_pose.x + linear_v*(1/control_frequenzy)*cos(current_pose.theta);
-    current_pose.y = current_pose.y + linear_v*(1/control_frequenzy)*sin(current_pose.theta);
-    current_pose.theta = current_pose.theta + angular_v*(1/control_frequenzy);
+
+    current_pose.x += linear_v*(cos(current_pose.theta)*dt;
+    current_pose.y += linear_v*(1/control_frequenzy)*sin(current_pose.theta)*dt;
+    current_pose.theta += angular_v*(dt);
+
+
+    tf::TransformBroadcaster odom_broadcaster;
+
+    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(current_pose.theta);
+    //first, we'll publish the transform over tf
+    geometry_msgs::TransformStamped odom_trans;
+    odom_trans.header.stamp = current_time;
+    odom_trans.header.frame_id = "odom";
+    odom_trans.child_frame_id = "base_link";
+
+    odom_trans.transform.translation.x = current_pose.x;
+    odom_trans.transform.translation.y = current_pose.y;
+    odom_trans.transform.translation.z = 0.0;
+    odom_trans.transform.rotation = odom_quat;
+
+    //send the transform
+    odom_broadcaster.sendTransform(odom_trans);
+
+    // Publish odometry message
+    nav_msgs::Odometry odom_msg;
+    odom_msg.header.stamp = current_time;
+    odom_msg.header.frame_id = "odom";
+
+    odom_msg.pose.pose.position.x = current_pose.x;
+    odom_msg.pose.pose.position.y = current_pose.y;
+    odom_msg.pose.pose.position.z = 0.0;
+    odom_msg.pose.pose.orientation = odom_quat;
+
+
 
 
     ROS_INFO("new Position [%f] [%f] [%f] ", current_pose.x, current_pose.y, current_pose.theta);
@@ -84,24 +122,19 @@ void calculateNewPosition(){
     ROS_INFO("Current power [%d], [%d]\n", pwr1, pwr2);
 
 }
-private:
 
-    std::vector<int> encoding_abs;
-    std::vector<double> encoding_delta;
-
-
-};
 
 int main(int argc, char **argv)
 {
 
-  ros::init(argc, argv, "Odometry_v1");
+  double frequency = 10;
+  ros::init(argc, argv, "odometry_publisher");
 
-  Odometry_v1 odom;
+  OdometryPublisher odom(frequency);
 
   ROS_INFO("Spin!");
 
-  ros::Rate loop_rate(10);
+  ros::Rate loop_rate(frequency);
 
   int count = 0;
   while (odom.n.ok()){
