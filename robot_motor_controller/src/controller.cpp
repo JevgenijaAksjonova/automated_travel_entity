c9268f9a6f13472d12d7487dbfcee3f50a8af0fd
EnyuCao <caoe@kth.se>
2017-10-05 20:00:53 +0200

    Update controller.cpp
    try to erase noise

--- robot_motor_controller/src/controller.cpp
+++ robot_motor_controller/src/controller.cpp
@@ -74,21 +74,13 @@ void pwmCalc(){
     ROS_INFO_STREAM( "left Error"<<error1<<"left Int Error:"<<(int)int_error1<<"pwm1:"<<(int)pwm1 );
     if (pwm1 > 50.0)   {pwm1 =  50.0; ROS_INFO_STREAM("PWM LIMITATION");}  //To protect the motor
     if (pwm1 < -50.0)  {pwm1 =  -50.0; ROS_INFO_STREAM("PWM LIMITATION");}   //To protect the motor
-    if ((pwm1 > -8.5)&&(pwm1 < 8.5))   {last_slow_time1 =ros::Time::now(); slow_flag1=1;}  //To protect the motor
-    if (slow_flag1 ==1 ){
-	if ((pwm1 < -8.5)||(pwm1 > 8.5)) slow_flag1 = 0;
-	    else if (ros::Time::now().toSec() - last_slow_time1.toSec() >3.0) {pwm1 =  0.0; ROS_INFO_STREAM("PWM SLOW");}
-    }
+
 	    //ROS_INFO_STREAM("leftPWM:"<<pwm1);
 
     ROS_INFO_STREAM( "Right Error"<<error2<<"Right Int Error:"<<(int)int_error2<<"pwm2:"<<(int)pwm2 );
     if (pwm2 > 50.0)   {pwm2 =  50.0; ROS_INFO_STREAM("PWM LIMITATION");}
     if (pwm2 < -50.0)  {pwm2 =  -50.0; ROS_INFO_STREAM("PWM LIMITATION");}
-    if ((pwm2 > -8.5)&&(pwm2 < 8.5))   {last_slow_time2 =ros::Time::now();slow_flag2=1;}  //To protect the motor
-    if (slow_flag2 ==1 ){
-	if ((pwm2 < -8.5)||(pwm2 > 8.5)) slow_flag2 = 0;
-	    else if (ros::Time::now().toSec() - last_slow_time2.toSec() >3.0) {pwm2 =  0.0; ROS_INFO_STREAM("PWM SLOW");}
-    }
+   
 	
 	
     //ROS_INFO_STREAM("RightPWM:"<<pwm2);
@@ -106,7 +98,11 @@ void motorMessageReceiverLeft( const phidgets::motor_encoder & msgRecEncoderLeft
   last_encoder_time_left =ros::Time::now();
   last_count_left = msgRecEncoderLeft.count;
   ROS_INFO_STREAM("leftReal:"<<motor.angular_velocity_left<<"leftRef:"<<reference.angular_velocity_left);
-
+    if (motor.angular_velocity_left  == 0)   {last_slow_time1 =ros::Time::now(); slow_flag1=1;}  //To protect the motor
+    if (slow_flag1 ==1 ){
+	if (motor.angular_velocity_left ！= 0) slow_flag1 = 0;
+	    else if (ros::Time::now().toSec() - last_slow_time1.toSec() >3.0) {pwm1 =  0.0; ROS_INFO_STREAM("Left SLOW,Stop");}
+    }
 }
 
 //Callback function 2: encoder right
@@ -119,6 +115,11 @@ void motorMessageReceiverRight( const phidgets::motor_encoder & msgRecEncoderRig
     last_count_right = msgRecEncoderRight.count;
     ROS_INFO_STREAM("rightReal:"<<motor.angular_velocity_right<<"rightRef:"<<reference.angular_velocity_right);
 
+     if (motor.angular_velocity_right ==0)   {last_slow_time2 =ros::Time::now();slow_flag2=1;}  //To protect the motor
+    if (slow_flag2 ==1 ){
+	if (motor.angular_velocity_right ！= 0) slow_flag2 = 0;
+	    else if (ros::Time::now().toSec() - last_slow_time2.toSec() >3.0) {pwm2 =  0.0; ROS_INFO_STREAM("Right SLOW， stop！");}
+    }	
 }
 
 
