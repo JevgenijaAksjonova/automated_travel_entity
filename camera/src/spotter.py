1b1765e89e98e5aee96ead6cdb347b5c26077079
someone@kth.se <someone@kth.se>
2017-10-06 16:01:21 +0200

    added rosbag for vision

--- camera/src/spotter.py
+++ camera/src/spotter.py
@@ -7,13 +7,20 @@ import cv2
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
 
+
+bridge = CvBridge()
+
 def image_callback(image):
-    pass
+    try:
+        cv_image = bridge.imgmsg_to_cv2(image)
+    except CvBridgeError as e:
+        rospy.logdebug(e)
+    
 def main():
     #Main fucntion. Put everything here
     pub = rospy.Publisher("/camera/object_candidates",PointStamped,queue_size=10)
     image_sub = rospy.Subscriber("/camera/rgb/image_color",Image,image_callback)
-    rospy.init_node("talker")
+    rospy.init_node("object_candidate_spotter")
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         foo = PointStamped()
@@ -22,10 +29,10 @@ def main():
         foo.point.x = 1
         foo.point.y = 2
         foo.point.z = 3
-        rospy.loginfo("gelo world")
+#        rospy.loginfo("gelo world")
         pub.publish(foo)
         rate.sleep()
-        
+ 
 if __name__ == "__main__":
     try:
 	    main()
