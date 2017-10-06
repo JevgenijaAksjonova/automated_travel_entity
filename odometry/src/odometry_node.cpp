eee80abdba461279110cdfa8b1dadf85310baa4a
Oskar Andersson <oanders@kth.se>
2017-10-06 11:28:17 +0200

    cleaned up the odometry code a bit

--- odometry/src/odometry_node.cpp
+++ odometry/src/odometry_node.cpp
@@ -6,7 +6,7 @@
 
 
 /**
- * This tutorial demonstrates simple sending of messages over the ROS system.
+ * The main node of the odometry frame
  */
 
 class OdometryPublisher{
@@ -15,11 +15,8 @@ public:
     ros::Publisher odom_publisher;
     ros::Subscriber encoder_subscriber_left;
     ros::Subscriber encoder_subscriber_right;
-    double xpos;
-    double ypos;
-    double theta;
-    double pi;
     tf::TransformBroadcaster odom_broadcaster;
+
 
 
 
@@ -27,15 +24,18 @@ OdometryPublisher(int frequency){
     control_frequency = frequency;
     n = ros::NodeHandle("~");
     pi = 3.1416;
-    xpos = 0;
-    ypos = 0;
-    theta = 0;
+    pos.x = 0;
+    pos.y = 0;
+    pos.theta = 0;
+    robot_params.base_diameter=0.26;
+    robot_params.ticks_per_rotation=900;
+    robot_params.wheel_radius=0.036;
 
 
     encoding_abs_prev = std::vector<int>(2,0);
     encoding_abs_new = std::vector<int>(2,0);
     encoding_delta = std::vector<double>(2,0);
-    first_loop = 1;
+    first_loop = true;
 
     odom_publisher = n.advertise<nav_msgs::Odometry>("odom", 1);
     encoder_subscriber_left = n.subscribe("/motorcontrol/encoder/left", 1, &OdometryPublisher::encoderCallbackLeft, this);
@@ -59,11 +59,8 @@ void encoderCallbackRight(const phidgets::motor_encoder::ConstPtr& msg){
 
 
 void calculateNewPosition(){
-    double wheel_r = 0.04;
-    double base_d = 0.25;
-    double tick_per_rotation = 900;
-    double control_frequenzy = 10; //10 hz
-    double dt = 1/control_frequenzy;
+
+    double dt = 1/control_frequency;
 
     ros::Time current_time = ros::Time::now();
 
@@ -73,7 +70,7 @@ void calculateNewPosition(){
         encoding_abs_prev[0] = encoding_abs_new[0];
         encoding_abs_prev[1] = encoding_abs_new[1];
     }
-    first_loop =0;
+    first_loop =false;
 
     encoding_delta[0] = encoding_abs_new[0] - encoding_abs_prev[0];
     encoding_delta[1] = encoding_abs_new[1] - encoding_abs_prev[1];
@@ -83,36 +80,36 @@ void calculateNewPosition(){
 
 
 
-    dphi_dt[0] = ((encoding_delta[0])/(tick_per_rotation)*2*pi)/dt;
-    dphi_dt[1] = ((encoding_delta[1])/(tick_per_rotation)*2*pi)/dt;
+    dphi_dt[0] = ((encoding_delta[0])/(robot_params.ticks_per_rotation)*2*pi)/dt;
+    dphi_dt[1] = ((encoding_delta[1])/(robot_params.ticks_per_rotation)*2*pi)/dt;
 
-    double linear_v = (wheel_r/2)*(dphi_dt[1] + dphi_dt[0]);
-    double angular_w = (wheel_r/base_d)*(dphi_dt[1] - dphi_dt[0]);
+    double linear_v = (robot_params.wheel_radius/2)*(dphi_dt[1] + dphi_dt[0]);
+    double angular_w = (robot_params.wheel_radius/robot_params.base_diameter)*(dphi_dt[1] - dphi_dt[0]);
 
-    double vx = linear_v*cos(theta);
-    double vy = linear_v*sin(theta);
+    double vx = linear_v*cos(pos.theta);
+    double vy = linear_v*sin(pos.theta);
 
 
-    xpos += linear_v*dt*cos(theta);
-    ypos += linear_v*dt*sin(theta);
-    theta += angular_w*dt;
-    if(theta > pi){
-        theta = theta-2*pi;
+    pos.x += linear_v*dt*cos(pos.theta);
+    pos.y += linear_v*dt*sin(pos.theta);
+    pos.theta += angular_w*dt;
+    if(pos.theta > pi){
+        pos.theta = pos.theta-2*pi;
     }
-    if(theta<-pi){
-        theta = theta+2*pi;
+    if(pos.theta<-pi){
+        pos.theta = pos.theta+2*pi;
     }
 
 
 
-    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(theta);
+    geometry_msgs::Quaternion odom_quat = tf::createQuaternionMsgFromYaw(pos.theta);
     geometry_msgs::TransformStamped odom_trans;
     odom_trans.header.stamp = current_time;
     odom_trans.header.frame_id = "odom";
     odom_trans.child_frame_id = "base_link";
 
-    odom_trans.transform.translation.x = xpos;
-    odom_trans.transform.translation.y = ypos;
+    odom_trans.transform.translation.x = pos.x;
+    odom_trans.transform.translation.y = pos.y;
     odom_trans.transform.translation.z = 0.0;
     odom_trans.transform.rotation = odom_quat;
 
@@ -123,8 +120,8 @@ void calculateNewPosition(){
     odom_msg.header.stamp = current_time;
     odom_msg.header.frame_id = "odom";
 
-    odom_msg.pose.pose.position.x = xpos;
-    odom_msg.pose.pose.position.y = ypos;
+    odom_msg.pose.pose.position.x = pos.x;
+    odom_msg.pose.pose.position.y = pos.y;
     odom_msg.pose.pose.position.z = 0.0;
     odom_msg.pose.pose.orientation = odom_quat;
 
@@ -136,7 +133,7 @@ void calculateNewPosition(){
 
     odom_publisher.publish(odom_msg);
 
-    ROS_INFO("new Position [%f] [%f] [%f] ", xpos, ypos, theta);
+    ROS_INFO("new Position [%f] [%f] [%f] ", pos.x, pos.y, pos.theta);
 
 }
 private:
@@ -144,7 +141,17 @@ private:
     std::vector<int> encoding_abs_new;
     std::vector<double> encoding_delta;
     int control_frequency;
-    int first_loop;
+    bool first_loop;
+    struct robot_parameters{
+        double wheel_radius;
+        double base_diameter;
+        double ticks_per_rotation;
+    } robot_params;
+    struct robot_position{
+        double x;
+        double y;
+        double theta;
+    }pos;
 };
 
 
