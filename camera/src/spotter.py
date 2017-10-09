480588290f93c314d4ecba9d2dfb4dfe49c6130f
Christoffer <christoffer.mockelind@live.se>
2017-10-09 20:26:24 +0200

    now have some basic spotting available

--- camera/src/spotter.py
+++ camera/src/spotter.py
@@ -12,10 +12,22 @@ bridge = CvBridge()
 
 hue_threshold = 80
 
+#Thresholds in hue for all collors
+colors = ["green","red","blue","yellow"]
 
-thresholds = {
-    "green" : (100,75)
+hue_thresholds = {
+    "green" : (125,75),
+    "red" : (255,0),
+    "blue" : (0,0),
+    "yellow" : (0,0) ,
 }
+color_2_rgb = {
+    "green" : (0,255,0),
+    "red" : (255,0,0),
+    "blue" : (0,0,255),
+    "yellow" : (0,255,255),
+}
+#Todo: add thresholds for value aswell, for increased robustness
 
 def image_callback(ros_image):
     try:
@@ -24,21 +36,22 @@ def image_callback(ros_image):
         rospy.logdebug(e)
     #lab_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2LAB)
     #YCrCb_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2YCrCb)
-    hsv_image =  cv2.cvtColor(rgb_image, cv2.COLOR_BGR2HSV)
-#    rospy.loginfo("uhsv_image[:,:,0].max() = " + str(hsv_image[:,:,0].max()))
-    
-    h_image = hsv_image[:,:,0]
-    max_thres = thresholds["green"][0]
-    min_thres = thresholds["green"][1]
-    rospy.loginfo("(max,min) = " + str((h_image.max(),h_image.min())))
-    idx_max = (h_image > max_thres) 
-    idx_min = (min_thres > h_image)
-    h_image[idx_max] = 0
-    h_image[idx_min] = 0
-    rospy.loginfo("(max_thres,min_thres) = " + str((max_thres,min_thres)))
-    ros_out_image = bridge.cv2_to_imgmsg(h_image,"mono8")
-    dbg_img_pub.publish(ros_out_image)
+    for color in colors: 
+        hsv_image =  cv2.cvtColor(rgb_image, cv2.COLOR_BGR2HSV)
+        h_image = hsv_image[:,:,0]
+        ret, thresh = cv2.threshold(h_image,hue_thresholds[color][1],hue_thresholds[color][0],cv2.THRESH_BINARY_INV)
+        contours,_ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        for contour in contours:
+            cv2.drawContours(rgb_image,[contour],-1,color=color_2_rgb[color],thickness=-1)
+            middle = (int(contour[:,0,0].mean()),int(contour[:,0,1].mean()))
+            cv2.circle(rgb_image,middle,radius=5,color=(0,0,0),thickness=2)
 
+            bot_right = (int(contour[:,0,0].max()),int(contour[:,0,1].max()))
+            top_left = (int(contour[:,0,0].min()),int(contour[:,0,1].min()))
+            cv2.rectangle(rgb_image,top_left,bot_right,color=(0,0,0),thickness=2)
+        #Find the center of the
+        ros_out_image = bridge.cv2_to_imgmsg(rgb_image,"rgb8")
+        dbg_img_pub.publish(ros_out_image)
 
 def main():
     #Main fucntion. Put everything here
