10ca1f0642ba8769a1b8abcb9afaa2e9ded439a1
Jevgenija <zeka_21@mail.ru>
2017-10-10 19:47:56 +0200

    ready for testing

--- navigation/src/navigation_node.cpp
+++ navigation/src/navigation_node.cpp
@@ -7,9 +7,13 @@
  *            jevaks <at> kth.se
  */
 
+#define _USE_MATH_DEFINES
+
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <nav_msgs/Odometry.h>
+#include <project_msgs/stop.h>
+#include <tf/transform_broadcaster.h>
 #include <sstream>
 #include <math.h>
 
@@ -34,6 +38,8 @@ void Location::callback(const nav_msgs::Odometry::ConstPtr& msg)
 {
   x = xStart + msg->pose.pose.position.x;
   y = yStart + msg->pose.pose.position.y;
+  geometry_msgs::Quaternion odom_quat = msg->pose.pose.orientation;
+  theta = tf::getYaw(odom_quat);
 }
 
 
@@ -44,7 +50,7 @@ class GoalPosition {
     double theta;
     bool changedPosition;
 
-    GoalPosition(): x(0), y(0), theta(0), changedPosition(false), changedAngle(false) {};
+    GoalPosition(): x(0), y(0), theta(0), changedPosition(false) {};
     void callback(const geometry_msgs::Twist::ConstPtr& msg);
 };
 
@@ -54,7 +60,7 @@ void GoalPosition::callback(const geometry_msgs::Twist::ConstPtr& msg)
   double y_new = msg->linear.y;
   double theta_new = msg->angular.x;
 
-  if (x_new != x || y_new != y || theta_new != theta}) {
+  if ((x_new != x)||(y_new != y)||(theta_new != theta)) {
       x = x_new;
       y = y_new;
       theta = theta_new;
@@ -74,43 +80,81 @@ class Path {
 
     vector<pair<double,double> > globalPath;
 
-    Path(): linVel(0), angVel(0), pathRad(0.05), distanceTol(0.01), move(false) {};
-    setGoal(double x, double y, double theta);
+    Path(): linVel(0), angVel(0), pathRad(0.05), distanceTol(0.01), angleTol(2*M_PI/360.0), move(false) {};
+    void setGoal(double x, double y, double theta);
     void followPath(double x, double y, double theta);
+    void obstaclesCallback(const project_msgs::stop::ConstPtr& msg);
   private:
     double pathRad;
     double distanceTol;
+    double angleTol;
     double goalX;
     double goalY;
     double goalAng;
     double distance(pair<double,double>& a, pair<double, double>& b);
+    double getAngle(pair<double,double> &g, pair<double, double> &p);
+    double diffAngles(double a, double b) ;
 };
 
-void setGoal(double x, double y, double theta) {
+void Path::setGoal(double x, double y, double theta) {
     goalX = x;
     goalY = y;
     goalAng = theta;
 }
 
-// squared Euclidean distane
+// Euclidean distane
 double Path::distance(pair<double, double> &a, pair<double, double> &b){
-    return pow(a.first-b.first, 2) + pow(a.second-b.second, 2);
+    return sqrt (pow(a.first-b.first, 2) + pow(a.second-b.second, 2) );
 }
 
-void Path::followPath(double x, double y) {
+// Returns angle in the interval (-Pi;Pi]
+double Path::getAngle(pair<double,double> &g, pair<double, double> &p) {
+    double x = g.first - p.first;
+    double y = g.second - p.second;
+    if (x == 0) {
+        if (y >= 0) {
+            return M_PI/2.0;
+        } else {
+            return -M_PI/2.0;
+        }
+    }
+    double angle = atan(y/x);
+    if (x > 0) {
+        return angle;
+    } else {
+        if (y >= 0) {
+            return angle + M_PI;
+        } else {
+            return angle - M_PI;
+        }
+    }
+}
+
+double Path::diffAngles(double a, double b) {
+    double diff = a-b;
+    if (diff > M_PI) {
+        return diff - 2*M_PI ;
+    } else if (diff <= - M_PI) {
+        return diff + 2*M_PI;
+    }
+    return diff;
+}
+
+void Path::followPath(double x, double y, double theta) {
     pair<double, double> loc(x,y);
+    pair<double,double> goal(goalX,goalY);
     if (globalPath.size() > 0 ) {
-        while (globalPath.size() > 0 && distance(globalPath[0],loc) < pow(pathRad,2)) {
+        while (globalPath.size() > 0 && distance(globalPath[0],loc) < pathRad) {
             globalPath.erase(globalPath.begin());
         }
-        linVel = sqrt(globalPath[0], loc);
-        angVel = //globalPath[0].second - loc.second;
-    } else if (distance(pair<double,double>(goalX,goalY),loc) < pow(distanceTol,2)) {
-        linVel = sqrt(pair<double,double>(goalX,goalY), loc);
-        angVel = //globalPath[0].second - loc.second;
-    } else if ( ) {
+        linVel = distance(globalPath[0], loc);
+        angVel = diffAngles(getAngle(globalPath[0],loc), theta);
+    } else if (distance(goal,loc) < distanceTol) {
+        linVel = distance(goal, loc);
+        angVel = getAngle(goal, loc);
+    } else if ( fabs(diffAngles(goalAng, theta)) < angleTol) {
         linVel = 0;
-        angVel = ;
+        angVel = diffAngles(goalAng, theta);
     } else {
         string msg = "Goal is reached!";
         ROS_INFO("%s/n", msg.c_str());
@@ -120,8 +164,11 @@ void Path::followPath(double x, double y) {
     }
 }
 
-void Path::obstaclesCallback() {
-
+void Path::obstaclesCallback(const project_msgs::stop::ConstPtr& msg) {
+    bool stop = msg->stop;
+    if (stop) {
+        move = false;
+    }
 }
 
 int main(int argc, char **argv)
@@ -162,7 +209,7 @@ int main(int argc, char **argv)
     }
 
     geometry_msgs::Twist msg;
-    msg.linear.x = path.xVel;
+    msg.linear.x = path.linVel;
     msg.linear.y = 0.0;
     msg.linear.z = 0.0;
     msg.angular.x = path.angVel;
