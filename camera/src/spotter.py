2e16d59e2070e16d9c7d56e0049f9a9e5c5ecdcd
Christoffer <christoffer.mockelind@live.se>
2017-10-11 12:00:55 +0200

    mocking entire object detection pipline

--- camera/src/spotter.py
+++ camera/src/spotter.py
@@ -10,6 +10,8 @@ import numpy as np
 
 bridge = CvBridge()
 
+#Set to true to get debug info
+
 hue_threshold = 80
 
 #Thresholds in hue for all collors
@@ -17,7 +19,7 @@ colors = ["green","red","blue","yellow"]
 
 hue_thresholds = {
     "green" : (125,75),
-    "red" : (255,0),
+    "red" : (35,0),
     "blue" : (0,0),
     "yellow" : (0,0) ,
 }
@@ -36,44 +38,62 @@ def image_callback(ros_image):
         rospy.logdebug(e)
     #lab_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2LAB)
     #YCrCb_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2YCrCb)
-    for color in colors: 
+    for color in ["green"]: 
         hsv_image =  cv2.cvtColor(rgb_image, cv2.COLOR_BGR2HSV)
         h_image = hsv_image[:,:,0]
+        
+        if DEBUGGING:
+            h_img_pub.publish(bridge.cv2_to_imgmsg(h_image,"mono8"))
+
         ret, thresh = cv2.threshold(h_image,hue_thresholds[color][1],hue_thresholds[color][0],cv2.THRESH_BINARY_INV)
         contours,_ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+        #Plot different hue values, see what rgb they correspond ti
+        #OR do it manually.
         for contour in contours:
-            cv2.drawContours(rgb_image,[contour],-1,color=color_2_rgb[color],thickness=-1)
-            middle = (int(contour[:,0,0].mean()),int(contour[:,0,1].mean()))
-            cv2.circle(rgb_image,middle,radius=5,color=(0,0,0),thickness=2)
-
+    
             bot_right = (int(contour[:,0,0].max()),int(contour[:,0,1].max()))
             top_left = (int(contour[:,0,0].min()),int(contour[:,0,1].min()))
-            cv2.rectangle(rgb_image,top_left,bot_right,color=(0,0,0),thickness=2)
-        #Find the center of the
-        ros_out_image = bridge.cv2_to_imgmsg(rgb_image,"rgb8")
-        dbg_img_pub.publish(ros_out_image)
+            middle = (int(contour[:,0,0].mean()),int(contour[:,0,1].mean()))
+    
+            #Mocking transform for now. OSKAR, yuor code goes here!
+            #Allso, we need to transform between depth frame and rgb frame here
 
+            obj_cand_msg = PointStamped()
+            obj_cand_msg.header.stamp = rospy.Time.now()
+            obj_cand_msg.header.frame_id = "/camera" #We might need to change this to it's propper value
+            obj_cand_msg.point.x = middle[0]
+            obj_cand_msg.point.y = middle[1]
+            obj_cand_msg.point.z = .1
+            obj_cand_pub.publish(obj_cand_msg)
+            
+            if DEBUGGING:    
+                cv2.drawContours(rgb_image,[contour],-1,color=color_2_rgb[color],thickness=-1)
+                cv2.circle(rgb_image,middle,radius=5,color=(0,0,0),thickness=2)
+                cv2.rectangle(rgb_image,top_left,bot_right,color=(0,0,0),thickness=2)
+            
+        if DEBUGGING:
+            ros_out_image = bridge.cv2_to_imgmsg(rgb_image,"rgb8")
+            dbg_img_pub.publish(ros_out_image)
+        
+         
+        
 def main():
     #Main fucntion. Put everything here
-    pub = rospy.Publisher("/camera/object_candidates",PointStamped,queue_size=10)
+    rospy.init_node("object_candidate_spotter")
+    global obj_cand_pub
+    obj_cand_pub = rospy.Publisher("/camera/object_candidates",PointStamped,queue_size=10)
+    
+    global DEBUGGING
+    DEBUGGING = rospy.get_param("/camera/debugging",True)
+    
     
     global dbg_img_pub
-    dbg_img_pub = rospy.Publisher("/camera/debug/hsv",Image,queue_size=1)
+    dbg_img_pub = rospy.Publisher("/camera/debug/img",Image,queue_size=1)
+    global h_img_pub
+    h_img_pub = rospy.Publisher("/camera/debug/hsv/h",Image,queue_size=1)
     
     image_sub = rospy.Subscriber("/camera/rgb/image_color",Image,image_callback)
-    rospy.init_node("object_candidate_spotter")
-    rate = rospy.Rate(10)
-    while not rospy.is_shutdown():
-        foo = PointStamped()
-        foo.header.frame_id = "camera"
-        foo.header.stamp = rospy.Time.now()
-        foo.point.x = 1
-        foo.point.y = 2
-        foo.point.z = 3
-#       rospy.loginfo("gelo world")
-        pub.publish(foo)
-        rate.sleep()
- 
+    rospy.spin() 
 if __name__ == "__main__":
     try:
 	    main()
