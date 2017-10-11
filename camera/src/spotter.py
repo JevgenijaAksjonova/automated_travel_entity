21c5b9575e7d1d6f2742447a6f8fe8c6fa33ff55
someone@kth.se <someone@kth.se>
2017-10-11 17:50:55 +0200

    Reworked spotter.py to have a main thread and not all the logic in the callback. Also added callback to get depth image and projection matrix

--- camera/src/spotter.py
+++ camera/src/spotter.py
@@ -6,6 +6,7 @@ from geometry_msgs.msg import PointStamped
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 from sensor_msgs.msg import Image
+from sensor_msgs.msg import CameraInfo
 import numpy as np
 
 bridge = CvBridge()
@@ -33,49 +34,64 @@ color_2_rgb = {
 
 def image_callback(ros_image):
     try:
+        global rgb_image
         rgb_image = bridge.imgmsg_to_cv2(ros_image)
+        global have_recieved_image
+        have_recieved_image = True
     except CvBridgeError as e:
         rospy.logdebug(e)
     #lab_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2LAB)
     #YCrCb_image = cv2.cvtColor(rgb_image, cv2.COLOR_BGR2YCrCb)
-    for color in ["green"]: 
-        hsv_image =  cv2.cvtColor(rgb_image, cv2.COLOR_BGR2HSV)
-        h_image = hsv_image[:,:,0]
-        
-        if DEBUGGING:
-            h_img_pub.publish(bridge.cv2_to_imgmsg(h_image,"mono8"))
 
-        ret, thresh = cv2.threshold(h_image,hue_thresholds[color][1],hue_thresholds[color][0],cv2.THRESH_BINARY_INV)
-        contours,_ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-        #Plot different hue values, see what rgb they correspond ti
-        #OR do it manually.
-        for contour in contours:
-    
-            bot_right = (int(contour[:,0,0].max()),int(contour[:,0,1].max()))
-            top_left = (int(contour[:,0,0].min()),int(contour[:,0,1].min()))
-            middle = (int(contour[:,0,0].mean()),int(contour[:,0,1].mean()))
-    
-            #Mocking transform for now. OSKAR, yuor code goes here!
-            #Allso, we need to transform between depth frame and rgb frame here
+# Get the depth image
+def depth_callback(ros_depth):
+    global depth_image
+    depth_image = bridge.imgmsg_to_cv2(ros_depth)
 
-            obj_cand_msg = PointStamped()
-            obj_cand_msg.header.stamp = rospy.Time.now()
-            obj_cand_msg.header.frame_id = "/camera" #We might need to change this to it's propper value
-            obj_cand_msg.point.x = middle[0]
-            obj_cand_msg.point.y = middle[1]
-            obj_cand_msg.point.z = .1
-            obj_cand_pub.publish(obj_cand_msg)
+# Get the projection matrix
+def info_callback(info_message):
+    global P
+    P = info_message.P
+
+def image_processing():
+    
+    if have_recieved_image:
+        for color in ["green"]: 
+            hsv_image =  cv2.cvtColor(rgb_image, cv2.COLOR_BGR2HSV)
+            h_image = hsv_image[:,:,0]
             
-            if DEBUGGING:    
-                cv2.drawContours(rgb_image,[contour],-1,color=color_2_rgb[color],thickness=-1)
-                cv2.circle(rgb_image,middle,radius=5,color=(0,0,0),thickness=2)
-                cv2.rectangle(rgb_image,top_left,bot_right,color=(0,0,0),thickness=2)
-            
-        if DEBUGGING:
-            ros_out_image = bridge.cv2_to_imgmsg(rgb_image,"rgb8")
-            dbg_img_pub.publish(ros_out_image)
-        
-         
+            if DEBUGGING:
+                h_img_pub.publish(bridge.cv2_to_imgmsg(h_image,"mono8"))
+
+            ret, thresh = cv2.threshold(h_image,hue_thresholds[color][1],hue_thresholds[color][0],cv2.THRESH_BINARY_INV)
+            contours,_ = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+            #Plot different hue values, see what rgb they correspond ti
+            #OR do it manually.
+            for contour in contours:
+
+                bot_right = (int(contour[:,0,0].max()),int(contour[:,0,1].max()))
+                top_left = (int(contour[:,0,0].min()),int(contour[:,0,1].min()))
+                middle = (int(contour[:,0,0].mean()),int(contour[:,0,1].mean()))
+
+                #Mocking transform for now. OSKAR, yuor code goes here!
+                #Allso, we need to transform between depth frame and rgb frame here
+
+                obj_cand_msg = PointStamped()
+                obj_cand_msg.header.stamp = rospy.Time.now()
+                obj_cand_msg.header.frame_id = "/camera" #We might need to change this to it's propper value
+                obj_cand_msg.point.x = middle[0]
+                obj_cand_msg.point.y = middle[1]
+                obj_cand_msg.point.z = .1
+                obj_cand_pub.publish(obj_cand_msg)
+                
+                if DEBUGGING:    
+                    cv2.drawContours(rgb_image,[contour],-1,color=color_2_rgb[color],thickness=-1)
+                    cv2.circle(rgb_image,middle,radius=5,color=(0,0,0),thickness=2)
+                    cv2.rectangle(rgb_image,top_left,bot_right,color=(0,0,0),thickness=2)
+                
+            if DEBUGGING:
+                ros_out_image = bridge.cv2_to_imgmsg(rgb_image,"rgb8")
+                dbg_img_pub.publish(ros_out_image)
         
 def main():
     #Main fucntion. Put everything here
@@ -92,10 +108,27 @@ def main():
     global h_img_pub
     h_img_pub = rospy.Publisher("/camera/debug/hsv/h",Image,queue_size=1)
     
+    global rgb_image
+
+    global have_recieved_image
+    have_recieved_image = False
+
     image_sub = rospy.Subscriber("/camera/rgb/image_color",Image,image_callback)
-    rospy.spin() 
+
+    depth_sub = rospy.Subscriber("/camera/depth/image_rect_raw",Image,depth_callback)
+
+    info_sub = rospy.Subscriber("/camera/rgb/camera_info",CameraInfo,info_callback)
+
+
+    rate = rospy.Rate(10)
+    rate.sleep()
+
+    while not rospy.is_shutdown():
+        image_processing()
+        rate.sleep()
+
 if __name__ == "__main__":
     try:
-	    main()
+        main()
     except rospy.ROSInterruptException:
         pass
