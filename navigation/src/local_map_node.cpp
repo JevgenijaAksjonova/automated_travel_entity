b453d3c20aafba3199b58412cd103a9eda1f82b8
someone@kth.se <someone@kth.se>
2017-11-06 14:03:54 +0100

    parameter changed

--- navigation/src/local_map_node.cpp
+++ navigation/src/local_map_node.cpp
@@ -188,7 +188,7 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "local_map_node");
     ros::NodeHandle nh;
 
-    LocalPathPlanner lpp(0.13, 0.25);
+    LocalPathPlanner lpp(0.13, 0.35);
     ros::ServiceServer service = nh.advertiseService("local_path", &LocalPathPlanner::amendDirection, &lpp);
     ros::Subscriber lidarSub = nh.subscribe("/scan", 1000, &LocalPathPlanner::lidarCallback, &lpp);
 
