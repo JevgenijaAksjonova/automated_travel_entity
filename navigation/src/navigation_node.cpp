3c8cb1afeb5a87d6ba307f20cd7bc48c1cf045ea
Jevgenija <zeka_21@mail.ru>
2017-11-23 23:44:51 +0100

    turnOnly if direction changed 3

--- navigation/src/navigation_node.cpp
+++ navigation/src/navigation_node.cpp
@@ -227,7 +227,7 @@ int main(int argc, char **argv)
         s << "Follow path " << path->linVel << " " << path->angVel << ", Location " << loc->x << " " << loc->y << " " << loc->theta;
         ROS_INFO("%s/n", s.str().c_str());
 
-        if (fabs(path->directionChanged) > 0.1) {
+        if (fabs(path->directionChange) > 0.1) {
             path->onlyTurn = true;
         } else if (path->onlyTurn && (path->angVel*prevAngVel <0 || path->angVel == 0) ) {
             // make sure that the robot turned enough (if sign differ, this is the case)
