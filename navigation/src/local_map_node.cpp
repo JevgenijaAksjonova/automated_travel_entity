3da92a6112da1aa0dace964b12ba3e22cf902ed8
someone@kth.se <someone@kth.se>
2017-11-24 14:53:31 +0100

    lpp radius increased

--- navigation/src/local_map_node.cpp
+++ navigation/src/local_map_node.cpp
@@ -75,7 +75,7 @@ void LocalPathPlanner::addRobotRadius(vector<double>& localMap){
         if (localMap[i] > 0) {
 
             int angAddMax = (asin((robotRad)/max(distance[i],robotRad))/2.0/M_PI*360);
-            int angAddMin = (asin((robotRad-0.04)/max(distance[i],robotRad-0.04))/2.0/M_PI*360);
+            int angAddMin = (asin((robotRad-0.05)/max(distance[i],robotRad-0.05))/2.0/M_PI*360);
             //cout << "(" << angAddMin << ":"<<angAddMax << ")" ;
             for (int di = -angAddMax; di < angAddMax+1; di++) {
                 int j = i + di;
@@ -304,7 +304,7 @@ int main(int argc, char **argv) {
     ros::init(argc, argv, "local_map_node");
     ros::NodeHandle nh;
 
-    LocalPathPlanner lpp(0.175, 0.25);
+    LocalPathPlanner lpp(0.18, 0.25);
     ros::ServiceServer service = nh.advertiseService("local_path", &LocalPathPlanner::amendDirection, &lpp);
     ros::Subscriber lidarSub = nh.subscribe("/scan", 1000, &LocalPathPlanner::lidarCallback, &lpp);
 
