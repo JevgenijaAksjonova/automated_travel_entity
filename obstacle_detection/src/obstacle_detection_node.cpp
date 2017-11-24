8623c2fa397cd882299672e28aa46d3b241d338a
Rickard Eriksson <rickard.e.1992@gmail.com>
2017-11-24 17:00:54 +0100

    fixed bugs

--- obstacle_detection/src/obstacle_detection_node.cpp
+++ obstacle_detection/src/obstacle_detection_node.cpp
@@ -1,113 +1,83 @@
-#include "ros/ros.h"
-
-#include <math.h>
-
+#include <ros/ros.h>
+// PCL specific includes
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
 
 
-/**
- * This tutorial demonstrates simple sending of messages over the ROS system.
- */
-using namespace std;
+ros::Publisher pub;
+
 
 class ObstaclePublisher
 {
   public:
     ros::NodeHandle n;
 
-    ros::Publisher obstacle_publisher;
+    // Create a ROS subscriber for the input point cloud
+    ros::Subscriber sub;
 
-    ros::Subscriber point_cloud_subscriber;
+    // Create a ROS publisher for the output point cloud
+    ros::Publisher pub;
 
-    FilterPublisher(int frequency)
+
+    ObstaclePublisher()
     {
         n = ros::NodeHandle("~");
-        /*
-        if(!n.getParam("/filter/particle_params/nr_particles",nr_particles)){
-            ROS_ERROR("failed to detect parameter 1");
-            exit(EXIT_FAILURE);
-        }
-        if(!n.getParam("/filter/particle_params/nr_measurements",nr_measurements)){
-            ROS_ERROR("failed to detect parameter 2");
-            exit(EXIT_FAILURE);
-        }
-        if(!n.getParam("/filter/particle_params/nr_random_particles",nr_random_particles)){
-            ROS_ERROR("failed to detect parameter 3");
-            exit(EXIT_FAILURE);
-        }
-        if(!n.getParam("/filter/particle_params/random_particle_spread",random_particle_spread)){
-            ROS_ERROR("failed to detect parameter 4");
-            exit(EXIT_FAILURE);
-        }
-        if(!n.getParam("/filter/particle_params/gaussian_particle_noise_spread",gaussian_particle_noise_spread)){
-            ROS_ERROR("failed to detect parameter 4");
-            exit(EXIT_FAILURE);
-        }
 
-        if(!n.getParam("/filter/particle_params/using_random_particles",using_random_particles)){
-            ROS_ERROR("failed to detect parameter 4");
-            exit(EXIT_FAILURE);
-        }
-        if(!n.getParam("/filter/odom_noise/k_D",k_D)){
-            ROS_ERROR("failed to detect parameter 5");
-            exit(EXIT_FAILURE);
-        }
-        if(!n.getParam("/filter/odom_noise/k_V",k_V)){
-            ROS_ERROR("failed to detect parameter 6");
-            exit(EXIT_FAILURE);
-        }
-        if(!n.getParam("/filter/odom_noise/k_W",k_W)){
-            ROS_ERROR("failed to detect parameter 7");
-            exit(EXIT_FAILURE);
-        }
-        */
+        sub = n.subscribe ("/camera/depth/points", 1, &ObstaclePublisher::pointCloudCallback, this);
 
-        obstacle_publisher = n.advertise<nav_msgs::Odometry>("/odom", 1);
-        point_cloud_subscriber = n.subscribe("/camera/depth/points", 1, &ObstaclePublisher::pointCloudCallback, this);
-        
-        
+        pub = n.advertise<sensor_msgs::PointCloud2> ("output", 1);
+
     }
 
-    void pointCloudCallback(const sensor_msgs::PointCloud2::ConstPtr &msg);
+    void pointCloudCallback(const sensor_msgs::PointCloud2ConstPtr& cloud_msg)
     {
-        ROS_INFO("Got point cloud data!");
+      // Container for original & filtered data
+      pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2; 
+      pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
+      pcl::PCLPointCloud2 cloud_filtered;
 
+      // Convert to PCL data type
+      pcl_conversions::toPCL(*cloud_msg, *cloud);
+
+      // Perform the actual filtering
+      pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
+      sor.setInputCloud (cloudPtr);
+      sor.setLeafSize (0.1, 0.1, 0.1);
+      sor.filter (cloud_filtered);
+
+      // Convert to ROS data type
+      sensor_msgs::PointCloud2 output;
+      pcl_conversions::fromPCL(cloud_filtered, output);
+
+      // Publish the data
+      pub.publish (output);
     }
 
-
-
-
-  private:
-    
-    int temp = 0;
 };
 
-int main(int argc, char **argv)
+
+int main (int argc, char** argv)
 {
 
-    ROS_INFO("Spin!");
+  ROS_INFO("Spinning!");
 
-    float frequency = 10;
-    
-    ros::init(argc, argv, "obstacle_detection_publisher");
+  // Initialize ROS
+  ros::init (argc, argv, "obstacle_detection");
 
+  ObstaclePublisher obs;
+  ros::Rate loop_rate(10);
+ 
+  int count = 0;
+  while (obs.n.ok())
+  {
+    ros::spinOnce();
 
-    ObstaclePublisher obstacle(frequency);
+    loop_rate.sleep();
+    ++count;
+  }
 
-    ros::Rate loop_rate(frequency);
-
-
-    int count = 0;
-    while (obstacle.n.ok())
-    {
-        ros::spinOnce();
-
-        loop_rate.sleep();
-        ++count;
-    }
-
-    return 0;
+  return 0;
 }
